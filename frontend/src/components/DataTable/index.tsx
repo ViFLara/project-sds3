ef9309abd34062d0f448e7a3d17c6828bea8fcb8
ViFLara <viviflara@gmail.com>
2021-05-07 14:14:35 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,84 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Seller</th>
+                        <th>Customers visited</th>
+                        <th>Closed deals</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr><tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>2021/04/22</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://www.linkedin.com/in/viviane-l-a84bab55/" target="_blank" rel="noreferrer">Viviane Lara</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+            Event promoted by DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
