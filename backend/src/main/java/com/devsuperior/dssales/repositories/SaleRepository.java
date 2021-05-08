0e6227642ca1fa1dc02fd347de75066156bc5a53
ViFLara <viviflara@gmail.com>
2021-05-08 15:41:25 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dssales/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dssales.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dssales.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+}
+
